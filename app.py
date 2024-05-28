783a8ca88bb7a9b5fdf1db79521a65c67b3df4b4
AlvinYN <74310468+AlvinYN@users.noreply.github.com>
2024-05-28 15:19:55 +0700

    Update print AKEi dan BBideal

--- app.py
+++ app.py
@@ -14,19 +14,20 @@ app = Flask(__name__)
 def api_rekomendasi():
     data = request.json
     Tb = data.get('tinggi_badan')
+    Bb = data.get('berat_badan')
     jenis_kelamin = data.get('jenis_kelamin')
     umur = data.get('umur')
     penyakit_input = data.get('penyakit', "").split(",")
     user_allergies = data.get('alergi', "").split(",")
 
-    if not Tb or not jenis_kelamin or not umur:
+    if not Tb or not jenis_kelamin or not umur or not Bb or not penyakit_input or not user_allergies:
         return jsonify({"error": "Invalid input"}), 400
 
     # Calculate ideal body weight
     berat_badan_ideal = hitung_berat_badan_ideal(Tb)
 
     # Calculate basic calorie needs
-    AKEi = hitung_AKEi_umur(berat_badan_ideal, jenis_kelamin, umur)
+    AKEi = hitung_AKEi_umur(Bb, Tb, jenis_kelamin, umur)
 
     # Prepare response data
     response_data = {
@@ -39,6 +40,9 @@ def api_rekomendasi():
     dataset_sarapan = dataset[dataset['Meal ID'] == 1]
     dataset_makan_siang = dataset[dataset['Meal ID'] == 2]
     dataset_makan_malam = dataset[dataset['Meal ID'] == 3]
+
+    print("Berat Badan Ideal (Bi):", berat_badan_ideal, "kg")
+    print("Angka Kebutuhan Energi Individu (AKEi) atau REE:", AKEi, "kalori/hari")
 
     # Menghitung dan menampilkan rekomendasi makanan untuk setiap waktu makan
     for meal_id, meal_name, dataset_mealtime in [(1, "sarapan", dataset_sarapan), (2, "makan siang", dataset_makan_siang), (3, "makan malam", dataset_makan_malam)]:
