c08402497328e83cbcc47056f25cfae5cdf6972d
AlvinYN <aallvviinn7@gmail.com>
2024-06-06 03:44:34 +0700

    Adding Rule-Based System, Filtering Recipe Code, and Newest Dataset

--- calculations.py
+++ calculations.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import NearestNeighbors
 
 # Load dataset
-dataset_path = "Cleaned_CombinedRecipe.csv"
+dataset_path = "newestUpdate_ConstrainsRecipe.csv"
 dataset = pd.read_csv(dataset_path)
 
 # Define allergy categories
